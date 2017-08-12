c743c767333c4daffef076ca08b45738572afc19
tattii <yuta.tatti@gmail.com>
2017-08-12 20:56:46 +0000

    delete odd station

---
+++ layers/railway/delete.sql
@@ -0,0 +1,10 @@
+-- ちんちん電車のりば
+delete from osm_railway_station where osm_id = 4487848586;
+delete from osm_railway_station where osm_id = 4487848587;
+
+-- 京都貨物駅
+delete from osm_railway_station where osm_id = 3164330519;
+
+-- 京都動物園
+delete from osm_railway_station where osm_id = 3164616938;
+
